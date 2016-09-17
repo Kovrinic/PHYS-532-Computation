7df4d1932b3066f647bb7c9f765133df00c68961
Kovrinic <rothfuss212+github@gmail.com>
2016-09-17 01:43:29 -0500

    chaged q to constant

--- CT3 (In Class).py
+++ CT3 (In Class).py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 import random
 
-# Electric Field is a measure of the physical realm of the affective strength of the (attractive/repulsive) force impartable to a charged particle by another system of charge, it is essentially a linear combination of charge and vector displacement to and from. 
+# Electric Field is a measure of the physical realm of the affective strength of the (attractive/repulsive) force impartable to a charged particle by another system of charge, it is essentially a linear combination of charge and vector displacement to and from.
 # E(r) =(1/4*pi*ep_0)*SUM{i=1->n}(q_i*(r^_i)/(r_i**2))
 
 
@@ -26,7 +26,7 @@ def Efield(q, r0, x, y):
     # Displacement vector uses trig components where r_x = (x-x0)/disp & r_y = (y-y0)/disp
     # E(x) = q*(1/(disp**2))*r_x => q*(x-x0)/(disp**1.5)
     # E(y) = q*(1/(disp**2))*r_y => q*(y-y0)/(disp**1.5)
-    
+
     disp = ((x - r0[0])**2 + (y - r0[1])**2) # where r0[0] = x0 & r0[1] = y0
     # Efield ends with returning output to python the field line values for all given x,y
     return q * (abs(x - r0[0])) / disp**1.5, q * (y - r0[1]) / disp**1.5
@@ -34,20 +34,20 @@ def Efield(q, r0, x, y):
 # To create distribution of a line of given length "L" of charge with linear charge density lambda (which is either given as lambda(r) or uniform lambda = Q/L) use numpy's linspace which generates a given number of points "steps" as a sequence of values in between a given a to b where "a" = -L/2 & "b"= L/2 (as to have line equidistantly split by origin along x with y = 0).
 # Define method "qDistribution" to create list of charge values at locations x,y
 def qDistribution():
-    dim = 2    
+    dim = 2
     L = 8*dim
-    steps = 512         
+    steps = 512
     nx, ny = steps, steps  #Setup number of samples to generate
     x = numpy.linspace(-dim, dim, nx)
     y = numpy.linspace(-dim, dim, ny)
     X, Y = numpy.meshgrid(x, y) # 2D plot space for E generated
 
     charges = list()
-    for i in range(0,steps):
-        q = (1/steps)*numpy.linspace((-L/2),(L/2),steps)
-        x0 = random.uniform((-L/2), (L/2)) # generates floating number between range stated in parentheses
-        y0 = 0
-        charges.append((q, (x0, y0)))
+    for i in x:
+        #q = (1/steps)*numpy.linspace((-L/2),(L/2),steps)
+        #x0 = random.uniform((-L/2), (L/2)) # generates floating number between range stated in parentheses
+        #y0 = 0
+        charges.append((1, (i, 0)))
 
 #Plotting (x,y) coordinates of defined field one by one, starting with E-Field array filled with zeroes for variable q at all (x,y)
     Ex, Ey = numpy.zeros((ny, nx)), numpy.zeros((ny, nx))
